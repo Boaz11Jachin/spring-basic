d1b1f710377a5df5d14fe8d495624708085f7954
Boaz11Jachin <sheepo84k@gmail.com>
2025-03-11 16:34:51 +0900

    feat : beanNameView 예시

--- src/main/java/org/codenova/spring/controller/IndexController.java
+++ src/main/java/org/codenova/spring/controller/IndexController.java
@@ -1,21 +1,31 @@
 package org.codenova.spring.controller;
 
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.servlet.ModelAndView;
 
 /*
     RequestMappingHandelrMapping 을 쓰게 되면 HttpRequestHandler 라는 interface 를 토대로 만들면 안됨.
     POJO (Plain Old Java Object) 형태로 Handler를 정의 내리면 되.
  */
-
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", "/index"})
-    public void index() {
+
+    @RequestMapping({"/" , "/index"})
+    public ModelAndView indexHandle() {
         System.out.println("IndexHandler.index");
+
+        //    ModelAndView mav = new ModelAndView();
+        //    mav.setViewName("index");
+
+        ModelAndView mav = new ModelAndView("indexView");
+        mav.addObject("msg", "HEllo Spring");   // req.setAttribute ..
+        mav.addObject("numbers", new int[]{11,2,3,4} );
+
+        return mav;
     }
 
     @RequestMapping("/help")
@@ -23,6 +33,6 @@ public class IndexController {
         System.out.println("IndexHandler.help");
     }
 
-
-
 }
+
+
