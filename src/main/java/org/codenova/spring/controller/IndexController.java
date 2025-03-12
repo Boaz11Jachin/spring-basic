ac28861e3b609a0639c88e18bfbb0ff966d39417
Boaz11Jachin <sheepo84k@gmail.com>
2025-03-12 10:06:07 +0900

    feat : path mapping 예시

--- src/main/java/org/codenova/spring/controller/IndexController.java
+++ src/main/java/org/codenova/spring/controller/IndexController.java
@@ -1,33 +1,27 @@
 package org.codenova.spring.controller;
 
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-
 
 /*
     RequestMappingHandelrMapping 을 쓰게 되면 HttpRequestHandler 라는 interface 를 토대로 만들면 안됨.
     POJO (Plain Old Java Object) 형태로 Handler를 정의 내리면 되.
  */
-
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", "/index"})
-    public ModelAndView indexHandle() {
+    @RequestMapping({"/" , "/index"})
+    public void indexHandle() {
         System.out.println("IndexHandler.index");
-        ModelAndView mav = new ModelAndView("index");
-        mav.addObject("msg", "Hello World");
-        return mav;
     }
 
     @RequestMapping("/help")
-    public String helpHandle() {
+    public void helpHandle() {
         System.out.println("IndexHandler.help");
-        return "help";
     }
 
-
-
 }
+
+
