fe32efc4e1ea4237fcc0331518c03d587efbaea9
Boaz11Jachin <sheepo84k@gmail.com>
2025-03-14 17:56:30 +0900

    feat : AOP 살펴보기 (미완)

--- src/main/java/org/codenova/spring/controller/practice/MovieController.java
+++ src/main/java/org/codenova/spring/controller/practice/MovieController.java
@@ -5,12 +5,26 @@ import org.codenova.spring.model.Movie;
 import org.codenova.spring.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
+
+
+    @ExceptionHandler(Exception.class)
+    public String exception () {
+        return "movie/error";
+        //        return "redirect:/index";
+    }
+
+
 
 
     @Autowired
@@ -28,6 +42,15 @@ public class MovieController {
     @RequestMapping("/form")
     public String movieFormHandle (){
         return "movie/form";
+    }
+
+
+    @RequestMapping("/list")
+    public String listHandle(@RequestParam int p, Model model){
+        List<Movie> movies = movieRepository.findAll();
+        model.addAttribute("movies", movies);
+
+        return "movie/list";
     }
 
 
