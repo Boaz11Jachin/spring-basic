befb09f08db63c32dfaa98e0852a98c0c1d48242
Boaz11Jachin <sheepo84k@gmail.com>
2025-03-17 12:00:00 +0900

    feat : 글로벌 예외처리

---
+++ src/main/java/org/codenova/spring/common/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package org.codenova.spring.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandle (Exception e){
+        return "error/exception";
+    }
+}
